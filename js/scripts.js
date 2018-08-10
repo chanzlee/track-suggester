ab4af6641ca75050719d919ac5cf1b8d48b89cff
Chan Lee <chanethanlee@gmail.com>
2018-08-10 12:51:39 -0700

    add modal, debugg applying length property on null value.

--- js/scripts.js
+++ js/scripts.js
@@ -13,18 +13,37 @@ $(document).ready(function() {
       alert("Please fill in the all blanks :)");
     } else {
 
+    // iAnswer concats all the string values from inputs
+
     var iAnswer = iWhere + iAcheive + iFeature1 + iFeature2 + iFeature3;
 
-    alert(iAnswer);
 
-    var countNet = iAnswer.match(/net/g).length;
-    var countRuby = iAnswer.match(/ruby/g).length;
-    var countReact = iAnswer.match(/react/g).length;
+    // To use length property on inAnswer.match, it should be checked whether it has null value, which causes error.
 
-    alert(countNet);
+    if (iAnswer.match(/net/g)) {
+      var countNet = iAnswer.match(/net/g).length;
+    } else {
+      var countNet = 0
+    }
+
+    if (iAnswer.match(/ruby/g)) {
+      var countRuby = iAnswer.match(/ruby/g).length;
+    } else {
+      var countRuby = 0
+    }
+
+    if (iAnswer.match(/react/g)) {
+      var countReact = iAnswer.match(/react/g).length;
+    } else {
+      var countReact = 0
+    }
+
+    // if null value is changed into 0, compare the number of answers related among Net, Ruby, Reactor and show most frequent answer.
+
 
     if (countNet >= countRuby && countNet >= countReact) {
       alert("net");
+      $(".net").toggle();
     } else if (countRuby >= countNet && countRuby >= countReact ) {
       alert("ruby");
     } else if (countReact >= countRuby && countReact >= countNet ) {
@@ -33,6 +52,8 @@ $(document).ready(function() {
       alert("error");
     }
 
+    // var nameInput = $("#name").val();
+    $("#submitModal").modal();
     }
     // close else of the first flow control, checking valid inputs.
   });
