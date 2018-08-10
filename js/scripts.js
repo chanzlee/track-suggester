5f3f1f32527be1248ef112644f254acf7f42e90a
Chan Lee <chanethanlee@gmail.com>
2018-08-10 11:12:02 -0700

    add questionaire, value on html, include jquery and bootstrap files.

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+
+$(document).ready(function() {
+
+  $("button#choose").click(function() {
+
+    // var iGender = parseInt($("#gender").val());
+    // var iHobby = parseInt($("#hobby").val());
+    //
+    // if (iGender === 1) {
+    //   if (iHobby === 1) {
+    //     $("img#4").toggle();
+    //     $(".intro").toggle();
+    //     $("#showtitle").toggle();
+    //   } else if (iHobby === 2) {
+    //     $("img#5").toggle();
+    //     $(".intro").toggle();
+    //     $("#showtitle").toggle();
+    //   } else if (iHobby === 3) {
+    //     $("img#6").toggle();
+    //     $(".intro").toggle();
+    //     $("#showtitle").toggle();
+    //   } else {
+    //     alert("not a number");
+    //   }
+    // } else if (iGender === 2) {
+    //   if (iHobby === 1) {
+    //     $("img#1").toggle();
+    //     $(".intro").toggle();
+    //     $("#showtitle").toggle();
+    //   } else if (iHobby === 2) {
+    //     $("img#2").toggle();
+    //     $(".intro").toggle();
+    //     $("#showtitle").toggle();
+    //   } else if (iHobby === 3) {
+    //     $("img#3").toggle();
+    //     $(".intro").toggle();
+    //     $("#showtitle").toggle();
+    //   } else {
+    //     alert("not a number");
+    //   }
+    // } else {
+    //   alert("not a number");
+    // }
+
+  });
+
+});
